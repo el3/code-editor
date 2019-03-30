09ff440f8f59ea7dea068b93fd0881b4b6e4fba5
Muhammad Khan <47133639+MBM1607@users.noreply.github.com>
2019-03-30 20:19:01 +0500

    Added basic layout, preview, saving and loading of kv files
    Creating a skeleton for the future development on the code editor by implementing vital features such as preview, saving and loading.

--- main.py
+++ main.py
@@ -1,43 +1,77 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.textinput import TextInput
+from kivy.uix.codeinput import CodeInput
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
+from kivy.extras.highlight import KivyLexer
+from kivy.properties import ObjectProperty
 
-KV = """
-BoxLayout
-    MyTextInput
-
-<MyTextInput>:
-    font_size: 24
-    text: 'This is my text input'
-
-"""
-
-class MyTextInput(TextInput):
+class CodeEditor(CodeInput):
+    lexer=KivyLexer()
 
     def keyboard_on_key_down(self, window, keycode, text, modifiers):
         self.readonly = False
-        
         if len(modifiers) and text:
-            
+
             if modifiers[0] == "ctrl" and ord(text) in [270, 269, 43, 45, 61]:
                 self.readonly = True
-                text = ord(text)
-
-                if text in [270, 43]:
+                if ord(text) in [270, 61, 43]:
                     self.font_size += 1
                     self.cursor = (self.cursor[0] - 1, 0)
                     return
-                if text in [269, 61, 45]:
+                elif ord(text) in [269, 45]:
                     self.font_size -= 1
                     self.cursor = (self.cursor[0] - 1, 0)
                     return
 
-        return super(MyTextInput, self).keyboard_on_key_down(window, keycode, text, modifiers)
-
+        return super(CodeEditor, self).keyboard_on_key_down(window, keycode, text, modifiers)
 
 class MyApp(App):
-    def build(self):
-        self.root = Builder.load_string(KV)
+    def __init__(self, **kwargs):
+        super(MyApp, self).__init__(**kwargs)
+        Window.bind(on_key_down=self.on_key_down)
+
+    def on_key_down(self, window, key, keycode, text, modifiers):
+        if len(modifiers) and text:
+            if modifiers[0] == "ctrl" and text in ["s", "l", "p"]:
+
+                if text == "s":
+                    self.save_file()
+                elif text == "l":
+                    self.load_file()
+                elif text == "p":
+                    self.preview()
+
+    def preview(self):
+        preview_area = self.root.preview_area
+        code_editor = self.root.code_editor
+        preview_area.clear_widgets()
+
+        try:
+            preview_area.add_widget(Builder.load_string(code_editor.text))
+        except Exception as e:
+            preview_area.add_widget(Label(text=(e.message if getattr(e, r"message", None) else str(e))))
+
+    def load_file(self):
+        code_editor = self.root.code_editor
+        filepath = self.root.filepath.text
+
+        try:
+            with open(filepath, "r", encoding="utf8") as file:
+                code_editor.text = file.read()
+        except Exception as e:
+            print("Couldn't load file", e)
+
+    def save_file(self):
+        code_editor = self.root.code_editor
+        filepath = self.root.filepath.text
+
+        try:
+            with open(filepath, "w", encoding="utf8") as file:
+                file.write(code_editor.text)
+        except Exception as e:
+            print("Couldn't save file", e)
 
 
-MyApp().run() 
+MyApp().run()
